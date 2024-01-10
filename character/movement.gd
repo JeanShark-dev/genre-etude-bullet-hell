ca47ef5fb84870528eef12dd72745b2fe1e50eb3
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-01-10 12:51:56 +0200

    started main menu

--- character/movement.gd
+++ character/movement.gd
@@ -5,8 +5,6 @@ const SPEED = 300.0
 
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
 	if direction:
 		velocity = direction * SPEED
