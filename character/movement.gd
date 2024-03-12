9b5de8978b36bdf62a57294201fa07d40848b753
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-03-12 12:29:18 +0200

    new movement script, resize handling, work on character sprite

--- character/movement.gd
+++ character/movement.gd
@@ -1,15 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+var speed = 100
 
 
 func _physics_process(delta):
+	var move_speed = speed
+	if Input.is_action_pressed("gp_focus"):
+		move_speed = speed/2
+	
 	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	
 	if direction:
-		velocity = direction * SPEED
+		velocity = direction * move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.y = move_toward(velocity.y, 0, move_speed)
 
 	move_and_slide()
