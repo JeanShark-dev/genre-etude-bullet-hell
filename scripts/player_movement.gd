a895ca4a616fd50c6aecd966769df6ff759835e5
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-04-24 10:25:18 +0300

    clamped player movement to þ game area

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -7,13 +7,15 @@ var type = "default"
 
 func _ready():
 	if type == "default":
-		speed = 400
+		speed = 200
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var moveSpeed = speed
+	$Hitbox.hide()
 	if Input.is_action_pressed("gp_focus"):
 		moveSpeed = speed/2
+		$Hitbox.show()
 	
 	var direction = Vector2(Input.get_axis("gp_left", "gp_right"), Input.get_axis("gp_up", "gp_down"))
 	
@@ -24,3 +26,4 @@ func _physics_process(delta):
 		velocity.y = move_toward(velocity.y, 0, moveSpeed)
 
 	move_and_slide()
+	position = position.clamp(Vector2(64,32), Vector2(800, 928))
