283111e357c35dcc5920b25b78815b057d0493a2
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-04-24 08:41:05 +0300

    connected main menu buttons to script, added quit button function

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,3 +9,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_option_pressed():
+	print("Starting game, please wait warmly...")
+
+
+func _on_extra_option_pressed():
+	print("Starting extra, please wait coolly...")
+
+
+func _on_records_option_pressed():
+	print("I rember")
+
+
+func _on_music_option_pressed():
+	print("best part of þ game")
+
+
+func _on_quit_option_pressed():
+	print("Bye!")
+	get_tree().quit()
