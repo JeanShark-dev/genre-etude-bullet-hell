9f957197dec1291fa1a681227423d22aeb41f6d3
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-04-25 12:46:41 +0300

    changed character to area2d for better hit detection

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,27 @@
-extends CharacterBody2D
+extends Area2D
 
 
 var speed = 0
 var type = "default"
-
+var moveSpeed
 
 func _ready():
 	if type == "default":
 		speed = 200
+	
+	moveSpeed = speed
 
 
-func _physics_process(_delta):
-	var moveSpeed = speed
-	$Hitbox.hide()
-	if Input.is_action_pressed("gp_focus"):
+func _physics_process(delta):
+	if Input.is_action_just_pressed("gp_focus"):
 		moveSpeed = speed/2
 		$Hitbox.show()
+	if Input.is_action_just_released("gp_focus"):
+		moveSpeed = speed
+		$Hitbox.hide()
 	
 	var direction = Vector2(Input.get_axis("gp_left", "gp_right"), Input.get_axis("gp_up", "gp_down"))
+	var velocity = direction * moveSpeed
+	position += velocity * delta
 	
-	if direction:
-		velocity = direction * moveSpeed
-	else:
-		velocity.x = move_toward(velocity.x, 0, moveSpeed)
-		velocity.y = move_toward(velocity.y, 0, moveSpeed)
-
-	move_and_slide()
 	position = position.clamp(Vector2(64,32), Vector2(800, 928))
