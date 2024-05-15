b048e57cc9052c7f01dfe04597d37907d419df50
Jean Shark <84136135+JeanShark-dev@users.noreply.github.com>
2024-05-15 09:49:16 +0300

    confirmed function of animationplayer

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -8,6 +8,7 @@ func _ready():
 	add_to_group("Enemy")
 
 
+
 func take_damage(damage):
 	healthPoints -= damage
 	if healthPoints <= 0:
--- scripts/stages/debugStage.gd
+++ scripts/stages/debugStage.gd
@@ -6,3 +6,14 @@ var stage = "debug"
 # enemies
 var spinner1 = load("res://scenes/enemies/spinner.tscn")
 
+
+func _ready():
+	$AnimationPlayer.play("stage") #needed for actually starting þ stage
+
+
+func _process(delta):
+	pass
+
+
+func test_funtion(testVariable):
+	print(testVariable)
